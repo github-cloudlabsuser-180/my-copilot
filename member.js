18d68a1ed726c177c7f6643ca8bf89fcda40b0a8
github-cloudlabsuser-180 <150350164+github-cloudlabsuser-180@users.noreply.github.com>
2023-12-19 16:01:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skills = {
+        name: 'John Smith',
+        age: 25,
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    console.log(skills.skills[2]);
+}
